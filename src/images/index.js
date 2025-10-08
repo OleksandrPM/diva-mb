668244cff49f9e98518af5aef03188fd0b1eb7f1
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-10-08 20:34:17 +0200

    Fix the svg-sprite URL

--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,5 @@
 import { teamPhotos, avatar as teamAvatar } from "./team";
 
-const icons = new URL("./icons.svg", import.meta.url);
+const icons = new URL("./icons.svg", import.meta.url).href;
 
 export { teamPhotos, icons, teamAvatar };
